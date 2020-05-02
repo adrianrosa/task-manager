1d552a2cef600f495c61bfeb7173d2d87192a731
adrian <adrianrosa93@gmail.com>
2020-05-02 02:10:12 -0300

    Add enpoint generic to undefined routes

--- api/server.js
+++ api/server.js
@@ -29,6 +29,11 @@ app.all('/*', function(req, res, next) {
 // Add routes
 app.use(`/`, routes);
 
+// Unhandled routes
+app.use((req, res, next) => {
+  res.status(404).json({error: true, data: `Malformed URL: ${req.url}`});
+});
+
 // DB connection
 global.db = db;
 db.init().then(() => console.log("DB connection success!"));
