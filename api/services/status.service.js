415a8ae93e02fa18201dc6f029e6e8bddd9890ff
adrian <adrianrosa93@gmail.com>
2020-06-17 22:16:14 -0300

    Fix status service on api

--- api/services/status.service.js
+++ api/services/status.service.js
@@ -1,8 +1,13 @@
 const statusModel = require('../models/status.model');
 
 const getStatusByName = async(name) => {
-    return statusModel.getByName(name)
-        .then(status => status[0]);
+	return statusModel.getByName(name)
+	.then(status => {
+		return {
+			id: status[0]._id,
+			name: status[0].name
+		};
+	});
 };
 
 exports.getStatusByName = getStatusByName;
